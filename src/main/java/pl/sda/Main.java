24b17eda4aff6e7f874d4bf064b0999bb23dd546
Krzysztof Żurada <kzurada@gmail.com>
2018-01-13 14:12:44 +0100

    napis od Krzyśka Ż

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -2,6 +2,7 @@ package pl.sda;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Napis od Krzyśka Ż");
         System.out.println("hello from master");
         System.out.println("hello world");
     }
