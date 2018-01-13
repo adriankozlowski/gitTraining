49457bd0fb4f378eb3ba488106d2baab67d3f80d
Krzysztof Żurada <kzurada@gmail.com>
2018-01-13 14:40:10 +0100

    napis2 od Krzyśka Ż

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Napis od Krzyśka Ż");
         System.out.println("hello from master");
         System.out.println("hello world");
+        System.out.println("hello Adrian");
+        System.out.println("Sprzedam krówkę");
+        System.out.println("nowy push");
     }
 }
